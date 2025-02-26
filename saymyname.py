0eac37fa64ef032a6e0cda3934e68342ce4aef6e
unknown <cherylclark90@live.co.uk>
2025-02-26 20:07:43 +0000

    messing with inputs

---
+++ saymyname.py
@@ -0,0 +1,11 @@
+# saymyname.py
+# messing with inputs
+# by Cheryl
+'''
+name = input("say your name:")
+print("hello " + name)
+'''
+
+number = int( input("enter a number: "))
+answer = number * 4
+print(answer)
