50fe47ae6b6e6f6fd10ce77a38ad30d0d59f11c5
unknown <cherylclark90@live.co.uk>
2025-03-12 16:18:11 +0000

    types

---
+++ messingwithtypes.py
@@ -0,0 +1,11 @@
+#messing with types
+
+
+anwser_of_question = "ahsdl"
+#answer_of_question = 11
+
+
+print (f"answer is {answer_of_question}")
+print ("answer is " + answer_of_question)
+print(f"type of answer is{type(answer_of_question)}")
+print (f"type of answer is {type(type(answer_of_question))}")
