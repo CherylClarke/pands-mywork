f2744d13e9335d4a5248888e96eb3f1bd7624865
unknown <cherylclark90@live.co.uk>
2025-04-18 21:37:08 +0100

    if staements

---
+++ testingif.py
@@ -0,0 +1,38 @@
+# program to show the format of an if statement 
+
+# format of an if statement
+# if condition:
+#   statements
+
+# if condition:
+#     statements(if true)
+# else:
+#     statements(if false)
+# 
+# 
+# if condition1:
+#      statements(if true)
+#  elif condition2:
+#      statements(if condition1 is false but 2 is true)
+# else:
+#     statement(if both false) 
+
+b =1
+if True:
+    print ("we are in the if statement")
+    b=2
+
+c = 1
+if c == 1:
+    print ("c is one"):
+else:
+    print ("c is not one")
+
+d = 4
+if d < 0:
+    print("d is negative")
+elif d >= 10:
+    print("d is 10 or higher")
+else:
+    print ("d is between 0 and 9(inclusive")
+
