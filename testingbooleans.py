440cc850f3c23097bb3146f374c52d60c5afc9b5
unknown <cherylclark90@live.co.uk>
2025-04-18 20:04:00 +0100

    practice

---
+++ testingbooleans.py
@@ -0,0 +1,9 @@
+# messing with booleans
+# booleans can be true or false
+# boolean is the return typeof the mathematical operators
+# = < > <= >=
+# flipping with the not keyword
+# be careful using and  or 
+
+
+is_alive = True
