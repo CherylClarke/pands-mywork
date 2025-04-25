dd94de5b9acdae0da1b018663ca2f93081b77cbb
unknown <cherylclark90@live.co.uk>
2025-04-25 12:24:20 +0100

    for testing

---
+++ testfor.py
@@ -0,0 +1,20 @@
+#  for elem in list:
+    # do something
+
+# for number in range(1,10):
+    #do something
+
+
+boats = ["sigma", "x yacht", "swan"]
+
+for boat in boats:
+    print("i'd rather be out in a ", boat)
+
+
+for i in range(0,10):
+    print (i)
+
+print ("all done i is now", i)
+
+if "swan" in boats:
+    print("that is a boat")
